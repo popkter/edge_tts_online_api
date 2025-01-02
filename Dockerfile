ae20fa34e6fabc7aa513dc078f5b74814da5b3d9
pop <popkter@outlook.com>
2025-01-02 15:34:27 +0800

    init

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 EXPOSE 10010
 
 # 运行命令
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "10010"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "10011"]
--- main.py
+++ main.py
@@ -1,60 +1,70 @@
-from typing import AsyncGenerator
+from enum import Enum
+from typing import AsyncGenerator, Optional
 
-import edge_tts
+import aiohttp
 import uvicorn
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+from pydantic import BaseModel
+
+
+class VoiceEmotion(Enum):
+    NORMAL = "me_f_y_1001_normal"
+    HAPPY = "me_f_y_1001_happy"
+    SAD = "me_f_y_1001_sad"
+    ANGRY_L1 = "me_f_y_1001_angryL1"
+    ANGRY_L2 = "me_f_y_1001_angryL2"
+    ANGRY_L3 = "me_f_y_1001_angryL3"
+
+
+# 定义请求模型
+class TTSRequest(BaseModel):
+    text: str
+    emotion: Optional[str] = VoiceEmotion.NORMAL.value
+    text_id: Optional[str] = "1111"
+    text_language: Optional[str] = "zh"
+
 
 app = FastAPI()
 
-async def generate_audio_chunks(text: str, voice: str, rate: int, volume: int) -> AsyncGenerator[bytes, None]:
-    if rate >= 0:
-        rates = f"+{rate}%"
-    else:
-        rates = f"{rate}%"
 
-    if volume >= 0:
-        volumes = f"+{volume}%"
-    else:
-        volumes = f"{volume}%"
+async def generate_audio_chunks(text: str, emotion: str, text_id: str, text_language: str) -> AsyncGenerator[
+    bytes, None]:
+    url = "http://14.103.16.83:29010/audio/tts"
+    api_key = "app-xKVUmRBNCVxbLt0q1ixZMyap"
 
-    communicate = edge_tts.Communicate(text, voice, rate=rates, volume=volumes)
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
 
-    async for chunk in communicate.stream():
-        if chunk["type"] == "audio":
-            yield chunk["data"]
+    # 请求主体
+    payload = {
+        "tts_model_name": emotion,
+        "text": text,
+        "text_id": text_id,
+        "text_language": text_language,
+        "ref_free": False,
+        "return_format": "wav_stream"}
 
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, headers=headers, json=payload) as response:
+            async for chunk in response.content.iter_chunked(32000):  # 使用较大的chunk size提高效率
+                if chunk:
+                    yield chunk
 
-@app.post("/synthesize")
-async def synthesize(request: Request):
-    data = await request.json()
-    text = data.get("text", "")
-    voice = data.get("voice", "Male-YunxiNeural")
-    rate = data.get("rate", 0)
-    volume = data.get("volume", 0)
-
+@app.get("/synthesize")
+async def synthesize(
+        text: str,
+        emotion: str = VoiceEmotion.NORMAL.value,
+        text_id: str = "1111",
+        text_language: str = "zh"
+):
     if not text:
-        return {"error": "Text is required"}
+        return {"error": "文本不能为空"}
 
-    return StreamingResponse(
-        generate_audio_chunks(text, voice, rate, volume),
-        media_type="audio/mp3"
-    )
-
-
-#http://localhost:10010/stream_audio?text=%E4%BB%8A%E5%A4%A9%E7%9A%84%E5%A4%A9%E6%B0%94%E6%80%8E%E4%B9%88%E6%A0%B7&voice=Female-XiaoxiaoNeural&rate=12&volume=0
-@app.get("/stream_audio")
-async def stream_audio(text: str, voice: str, rate: int, volume: int):
-    if not text:
-        raise HTTPException(status_code=400, detail="Text Parameter is required")
-    if not voice:
-        voice = 'Female-XiaoxiaoNeural'
-    if not rate:
-        rate = 12
-    if not volume:
-        volume = 0
-    return StreamingResponse(generate_audio_chunks(text, voice, rate, volume), media_type="audio/mp3")
+    return StreamingResponse(generate_audio_chunks(text, emotion, text_id, text_language), media_type="audio/mp3")
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=10010)
+    uvicorn.run(app, host="0.0.0.0", port=10011)
