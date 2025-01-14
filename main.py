71360b290f3062b02106f5c2aa1df10cc4ea3b3a
pop <popkter@outlook.com>
2025-01-14 14:01:22 +0800

    init volcengine tts

--- main.py
+++ main.py
@@ -1,76 +1,127 @@
-from enum import Enum
-from typing import AsyncGenerator, Optional
-
-import aiohttp
+import websockets
+import uuid
+import json
+import gzip
+from fastapi import FastAPI, Response
 import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import StreamingResponse
-from pydantic import BaseModel
 from dotenv import load_dotenv
 import os
 
+MESSAGE_TYPES = {11: "audio-only server response", 12: "frontend server response", 15: "error message from server"}
+MESSAGE_TYPE_SPECIFIC_FLAGS = {0: "no sequence number", 1: "sequence number > 0",
+                               2: "last message from server (seq < 0)", 3: "sequence number < 0"}
+MESSAGE_SERIALIZATION_METHODS = {0: "no serialization", 1: "JSON", 15: "custom type"}
+MESSAGE_COMPRESSIONS = {0: "no compression", 1: "gzip", 15: "custom compression method"}
+
 load_dotenv()
-SENSE_TTS_API_KEY =os.getenv("SENSE_TTS_API_KEY")
+appid = os.getenv("APP_ID")
+token =  os.getenv("TOKEN")
+cluster = "volcano_tts"
+
+host = "openspeech.bytedance.com"
+api_url = f"wss://{host}/api/v1/tts/ws_binary"
+
+default_header = bytearray(b'\x11\x10\x11\x00')
+
+async def stream_tts(request_json):
+    payload_bytes = str.encode(json.dumps(request_json))
+    payload_bytes = gzip.compress(payload_bytes)
+    full_client_request = bytearray(default_header)
+    full_client_request.extend((len(payload_bytes)).to_bytes(4, 'big'))
+    full_client_request.extend(payload_bytes)
+
+    audio_data = bytearray()  # 用于存储音频数据
+    header = {"Authorization": f"Bearer; {token}"}
+    async with websockets.connect(api_url, additional_headers=header, ping_interval=None) as ws:
+        await ws.send(full_client_request)
+        while True:
+            res = await ws.recv()
+            done = parse_response(res, audio_data)
+            if done:
+                break
+        ws.close()
+    return bytes(audio_data)
 
 
-class VoiceEmotion(Enum):
-    NORMAL = "me_f_y_1001_normal"
-    HAPPY = "me_f_y_1001_happy"
-    SAD = "me_f_y_1001_sad"
-    ANGRY_L1 = "me_f_y_1001_angryL1"
-    ANGRY_L2 = "me_f_y_1001_angryL2"
-    ANGRY_L3 = "me_f_y_1001_angryL3"
-
-
-
-
-# 定义请求模型
-class TTSRequest(BaseModel):
-    text: str
-    emotion: Optional[str] = VoiceEmotion.NORMAL.value
-    text_id: Optional[str] = "1111"
-    text_language: Optional[str] = "zh"
+def parse_response(res, audio_data: bytearray):
+    print("--------------------------- response ---------------------------")
+    header_size = res[0] & 0x0f
+    message_type = res[1] >> 4
+    message_type_specific_flags = res[1] & 0x0f
+    message_compression = res[2] & 0x0f
+    header_extensions = res[4:header_size * 4]
+    payload = res[header_size * 4:]
+    if header_size != 1:
+        print(f"           Header extensions: {header_extensions}")
+    if message_type == 0xb:  # audio-only server response
+        if message_type_specific_flags == 0:
+            return False
+        else:
+            sequence_number = int.from_bytes(payload[:4], "big", signed=True)
+            payload_size = int.from_bytes(payload[4:8], "big", signed=False)
+            payload = payload[8:]
+            audio_data.extend(payload)  # 追加音频数据
+            if sequence_number < 0:
+                return True
+            return False
+    elif message_type == 0xf:
+        code = int.from_bytes(payload[:4], "big", signed=False)
+        msg_size = int.from_bytes(payload[4:8], "big", signed=False)
+        error_msg = payload[8:]
+        if message_compression == 1:
+            error_msg = gzip.decompress(error_msg)
+        error_msg = str(error_msg, "utf-8")
+        print(f"          Error message code: {code}")
+        print(f"          Error message size: {msg_size} bytes")
+        print(f"               Error message: {error_msg}")
+        return True
+    elif message_type == 0xc:
+        msg_size = int.from_bytes(payload[:4], "big", signed=False)
+        payload = payload[4:]
+        if message_compression == 1:
+            payload = gzip.decompress(payload)
+        print(f"            Frontend message: {payload}")
+    else:
+        print("undefined message type!")
+        return True
 
 
 app = FastAPI()
 
 
-async def generate_audio_chunks(text: str, emotion: str, text_id: str, text_language: str) -> AsyncGenerator[
-    bytes, None]:
-    url = "http://14.103.16.83:29010/audio/tts"
-
-    headers = {
-        "Authorization": f"Bearer {SENSE_TTS_API_KEY}",
-        "Content-Type": "application/json"
-    }
-
-    # 请求主体
-    payload = {
-        "tts_model_name": emotion,
-        "text": text,
-        "text_id": text_id,
-        "text_language": text_language,
-        "ref_free": False,
-        "return_format": "wav_stream"}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.post(url, headers=headers, json=payload) as response:
-            async for chunk in response.content.iter_chunked(32000):  # 使用较大的chunk size提高效率
-                if chunk:
-                    yield chunk
-
 @app.get("/synthesize")
 async def synthesize(
         text: str,
-        emotion: str = VoiceEmotion.NORMAL.value,
-        text_id: str = "1111",
+        emotion: str = "BV705_streaming",
+        text_id: str = str(uuid.uuid4()),
         text_language: str = "zh"
 ):
-    if not text:
-        return {"error": "文本不能为空"}
-
-    return StreamingResponse(generate_audio_chunks(text, emotion, text_id, text_language), media_type="audio/mp3")
+    request_json = {
+        "app": {
+            "appid": appid,
+            "token": "access_token",
+            "cluster": cluster
+        },
+        "user": {
+            "uid": "388808087185088"
+        },
+        "audio": {
+            "voice_type": emotion,
+            "encoding": "mp3",
+            "speed_ratio": 1.0,
+            "volume_ratio": 1.0,
+            "pitch_ratio": 1.0,
+        },
+        "request": {
+            "reqid": text_id,
+            "text": text,
+            "text_type": "plain",
+            "operation": "submit"
+        }
+    }
+    audio_data = await stream_tts(request_json)
+    return Response(content=audio_data, media_type="audio/mp3")
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=10011)
+    uvicorn.run(app, host="0.0.0.0", port=10012)
